2adb85ad3578467f74241628f7186f835744feba
Eric Meas <emmeas@gmail.com>
2022-10-03 12:35:09 -0700

    Update amc_api.py
    Creating skeleton to retrieve API data

--- amc_api.py
+++ amc_api.py
@@ -3,3 +3,18 @@ amc_api.py:
 Scraping AMC API from: https://www.stockdata.org/
 using Python
 """
+
+import webbrowser
+
+import requests
+from requests.exceptions import HTTPError
+from requests import get
+
+api_key = input("API Key: ")
+url = (f""
+        "{api_key}")
+
+r = requests.get(url)
+data = r.raw.read()
+
+print(data)
