e3687c5960cb624a06237ff012b6ffc9cf2a6cc0
Eric Meas <emmeas@gmail.com>
2022-10-03 16:46:21 -0700

    Update amc_api.py
    Added for loop and moved response inside. Also added link error checking and creating a JSON file with the API data

--- amc_api.py
+++ amc_api.py
@@ -6,6 +6,7 @@ using Python
 
 import webbrowser
 
+import json
 import requests
 from requests.exceptions import HTTPError
 from requests import get
@@ -14,4 +15,16 @@ api_key = input("API Key: ")
 link = ("https://api.stockdata.org/v1/data/eod?"
         "symbols=AMC&api_token={}".format(api_key))
 
-response = requests.get(url)
+for url in [link]:
+  try:
+    response = requests.get(url)
+    json_data = response.json()
+    response.raise_for_status()
+  except HTTPError as http_err:
+    print(f"HTTP error occurred: {http_err}")
+  except Exception as err:
+    print(f"Other error occurred: {err}")
+  else:
+    print("Success!")
+    json.dump(json_data, open("AMC_data.json", "w"))
+    file.close()
