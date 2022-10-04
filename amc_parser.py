6f9801fcf9f1a07f84f0dde7f2c22bfd400b1b21
Eric Meas <emmeas@gmail.com>
2022-10-04 09:22:29 -0700

    Update amc_parser.py
    sorting and editing dataframe data into something more viewable

--- amc_parser.py
+++ amc_parser.py
@@ -12,3 +12,12 @@ with open("AMC_data.json", "r") as infile:
 
   #putting data in a dataframe
   df = pd.DataFrame(amc_data["data"])
+
+  #removing excess parts from date column
+  df["date"] = df["date"].map(lambda x: str(x)[:-17])
+
+  #sorting date column by ascending and reseting row count 1-250
+  sort_df = df.sort_values(by="date", ascending=True).reset_index(drop=True)
+
+  #changing column order to date > open > high > low > close > volume
+  re_df = sort_df[["date","open","high","low","close","volume"]]
