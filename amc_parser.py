eb87c286b5870a71c66ebffa45504738440d7b80
Eric Meas <emmeas@gmail.com>
2022-10-04 08:09:07 -0700

    Update amc_parser.py
    adding pandas and creating a dataframe with the data

--- amc_parser.py
+++ amc_parser.py
@@ -4,7 +4,11 @@ Parsing scraped AMC API data using Python
 """
 
 import json
+import pandas as pd
 
 #opening AMC json data
 with open("AMC_data.json", "r") as infile:
   amc_data = json.load(infile)
+
+  #putting data in a dataframe
+  df = pd.DataFrame(amc_data["data"])
